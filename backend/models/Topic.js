ecd4faa75a4b09b6924907504129120f286d3a31
akshitsaini2003 <151360997+akshitsaini2003@users.noreply.github.com>
2025-03-19 00:04:56 +0530

    Delete backend directory

--- backend/models/Topic.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const topicSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  topicId: { type: mongoose.Schema.Types.ObjectId, default: () => new mongoose.Types.ObjectId(), unique: true }
-});
-const Topic = mongoose.model('Topic', topicSchema);
-module.exports = Topic;
